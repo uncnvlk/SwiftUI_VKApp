3a406f0072d592cb6848a73aa0060176e28b7456
lisa <elizavetasidorenkova@MacBook-Air-Elizaveta.local>
2022-02-07 20:33:19 +0300

    login screen

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Elizaveta Sidorenkova on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
